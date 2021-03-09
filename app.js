48d1ce1c467351afcc0c7397b355d5e7f5d3fd09
harshinvader <emails.harsha@gmail.com>
2021-03-09 11:52:22 +0530

    added folder

--- app.js
+++ app.js
@@ -493,3 +493,19 @@ barba.init({
   ],
 });
 gsap.config({ nullTargetWarn: false });
+
+// Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
