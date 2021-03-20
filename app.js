6f2a459c144c7c8349dfbf1dece1e7d9795ead0f
Shivanshu Singh <43378694+ShivS01@users.noreply.github.com>
2021-03-20 18:47:35 +0530

    Adding CognoAI to sparkathon (#3)
    * Adding CognoAI to sparkathon
    
    * Remove console logs
    
    * text align center and increase dropdown box width for smaller screens
    
    * Update problem statement

--- app.js
+++ app.js
@@ -437,10 +437,26 @@ function faqAccordion() {
   }
 }
 
+themeDropBox = () => {
+  theme = document.querySelector("#CognoAI");
+  theme.addEventListener("click", () => {
+    theme.classList.toggle("open");
+    panel = theme.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  })
+
+}
+
 document.addEventListener("DOMContentLoaded", animatePages());
 document.addEventListener("DOMContentLoaded", animateThings());
-// document.addEventListener("DOMContentLoaded", faqAccordion());
+document.addEventListener("DOMContentLoaded", themeDropBox());
 
+// document.addEventListener("DOMContentLoaded", faqAccordion());
+// document.addEventListener("DOMContentLoaded", themeDropBox());
 // document.addEventListener("DOMContentLoaded", skewEffect());
 
 barba.init({
